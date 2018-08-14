eb03a79082b8564a3cfa9d1225f7613fca9d3def
gdev219 <gdev219@gmail.com>
2018-08-14 17:19:25 +0900

    post page changed

---
+++ store/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+// window.fetch()를 위한 Polyfill
+require('whatwg-fetch');
+
+const store = () => new Vuex.Store({
+
+
+  state: {
+    authUser: null
+  },
+
+  mutations: {
+    SET_USER: function (state, user) {
+      state.authUser = user
+    }
+  },
+
+  actions: {
+    // ...
+  }
+
+})
+
+export default store
